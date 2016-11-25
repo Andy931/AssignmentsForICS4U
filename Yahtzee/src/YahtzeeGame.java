03c1a4d734962a4afa87f6b9a6428ce834a04b89
Andy931 <andy.liu@ocsbstudent.ca>
2016-11-24 21:23:39 -0500

    First Commit: project begins

---
+++ Yahtzee/src/YahtzeeGame.java
@@ -0,0 +1,17 @@
+/****************************************************************************
+ *
+ * Created by: Andy Liu
+ * Created on: Nov 24, 2016
+ * Created for: ICS4U
+ * Assignment #6b
+ * This program is a class for Yahtzee Game
+ *
+ ****************************************************************************/
+
+public class YahtzeeGame {
+	public static int[] arrayOfDice;
+	public static int[] scorePad;
+	
+	//Constructor
+	public
+}
